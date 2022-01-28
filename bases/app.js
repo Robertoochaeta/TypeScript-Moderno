f18128d1fc17b89a40e15a2603b11e112ad3e854
Roberto <mario24roberto@gmail.com>
2022-01-27 21:17:06 -0600

    Agregando un tipo de dato 10

--- bases/app.js
+++ bases/app.js
@@ -1,10 +1,2 @@
 "use strict";
-const msg = 'Hola Mundo!!!';
-// msg = 'Roberto';
-// msg = 123;
-const heroe = {
-    name: 'Iron Man',
-    age: 45
-};
-heroe.age = 50;
-console.log(heroe.age + 1);
+const a = 10;
--- bases/app.ts
+++ bases/app.ts
@@ -1,9 +1,1 @@
-const msg: string = 'Hola Mundo!!!';
-// msg = 'Roberto';
-// msg = 123;
-const heroe={
-    name:'Iron Man',
-    age:45
-}
-heroe.age=50;
-console.log(heroe.age+1);
+const a = 10;
