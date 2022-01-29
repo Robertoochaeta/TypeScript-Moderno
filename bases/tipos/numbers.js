1e6942de185be882298bc8bb32ecf81be8e167e0
Roberto <mario24roberto@gmail.com>
2022-01-28 21:43:17 -0600

    Agregnando la función Number

---
+++ bases/tipos/numbers.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    let avengers = 150;
+    console.log(avengers);
+    const villanos = 20;
+    if (avengers < villanos) {
+        console.log("Estamos en problemas");
+    }
+    else {
+        console.log("Nos salvamos");
+    }
+    avengers = Number('123');
+    console.log({ avengers });
+})();
---
+++ bases/tipos/numbers.ts
@@ -0,0 +1,14 @@
+(()=>{
+
+    let avengers:number=150; 
+    console.log(avengers)
+    const villanos :number= 20;
+    if(avengers < villanos){
+        console.log("Estamos en problemas")
+
+    }else{
+        console.log("Nos salvamos")
+    }
+    avengers = Number('123');
+    console.log({avengers})
+    })()
