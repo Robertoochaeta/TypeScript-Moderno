2ed5dbff607045f4af80e9f8bb6e5b064d20143c
Roberto <mario24roberto@gmail.com>
2022-06-25 10:43:03 -0600

    Creando variables de tipo any

---
+++ bases/tipos/any.ts
@@ -0,0 +1,13 @@
+(()=>{
+    let avenger:any = 123;
+    let exist;
+    let power;
+    avenger = 'Doctor Stranger';
+    console.log(avenger.charAt(0));
+
+    avenger = 150.666631214;
+    console.log(avenger.toFixed(2))
+
+    console.log(exist);
+    console.log(power)
+})()
